305fe343f20bd92b4a500c5d71584e89ef1db549
Evan Beck <evandrydenbeck@gmail.com>
2023-05-02 19:58:01 -0400

    complete functional counter app

--- Day-1-Counter-App/script.js
+++ Day-1-Counter-App/script.js
@@ -1,2 +1,16 @@
 let fileName = window.location.href.split('/').slice(0, -1).pop();
 document.title = fileName;
+
+const count = document.querySelector("p");
+const buttons = document.querySelector("div :nth-child(4)")
+
+buttons.addEventListener("click", (e) => {
+    let event = e.target.innerHTML;
+    if (event === "Add") {
+        count.innerHTML++;
+    } else if (event === "Subtract") {
+        count.innerHTML--;
+    } else if (event === "Reset") {
+        count.innerHTML = 0;
+    }
+});
