91f6ebd4baa5137ccb0e97a5a4a66a55b7f00c82
Evan Beck <evandrydenbeck@gmail.com>
2023-05-04 14:18:20 -0400

    add functioning RNG

--- Day-2-Random-Number-Generator/script.js
+++ Day-2-Random-Number-Generator/script.js
@@ -1,2 +1,10 @@
 let fileName = window.location.href.split('/').slice(0, -1).pop();
 document.title = fileName;
+
+const number = document.querySelector("p");
+const generate = document.querySelector("div :nth-child(4)")
+
+generate.addEventListener("click", () => {
+    let rand = Math.floor(Math.random() * 10)
+    number.innerHTML = rand;
+});
