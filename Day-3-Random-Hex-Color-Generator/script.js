38eac6cbf31e2976dcc500d92228f8486997ae64
Evan Beck <evandrydenbeck@gmail.com>
2023-05-08 19:57:50 -0400

    add functional hex color generator

--- Day-3-Random-Hex-Color-Generator/script.js
+++ Day-3-Random-Hex-Color-Generator/script.js
@@ -1,2 +1,11 @@
 let fileName = window.location.href.split('/').slice(0, -1).pop();
 document.title = fileName;
+
+const hex = document.querySelector("p");
+const generate = document.querySelector("div :nth-child(4)")
+
+generate.addEventListener("click", () => {
+    let color = Math.random().toString(16).substring(2, 8);
+    hex.innerHTML = color;
+    document.body.style.backgroundColor = "#" + color
+})
